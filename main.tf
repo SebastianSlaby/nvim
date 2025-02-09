ff6344b41ab31e1293ebd87ff34f8102562dd3ee
Sebastian Słaby <sebastianslaby98@gmail.com>
2025-02-10 00:18:07 +0100

    conform for go

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.86.0"
+    }
+  }
+}
+
+
+
+provider "aws" {
+  access_key = "AWS ACCESS KEY"
+  secret_key = "AWS SECRET KEY"
+  region     = "AWS REGION"
+}
+
