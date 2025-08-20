92edcf1f780c95ede9edca3cc7b0c82c52e1c7bb
Sebastian Słaby <sebastianslaby98@gmail.com>
2025-08-20 21:41:37 +0200

    fuzzy finder

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,6 +10,8 @@ vim.cmd [[
   Plug 'nvim-tree/nvim-tree.lua'
   Plug 'waiting-for-dev/ergoterm.nvim'
   Plug 'anuvyklack/hydra.nvim'
+  Plug 'nvim-lua/plenary.nvim'
+  Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.8' }
   call plug#end()
 ]]
 
@@ -150,3 +152,9 @@ require("claude-code").setup({
     scrolling = true,         -- Enable scrolling keymaps (<C-f/b>) for page up/down
   }
 })
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
