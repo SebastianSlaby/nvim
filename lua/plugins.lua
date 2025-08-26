a4a58311475a72eccff944b3c09848b40cac2d1d
Sebastian Słaby <sebastian.slaby@hyperexponential.com>
2025-08-26 14:48:47 +0200

    gitlab ci lsp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -199,7 +199,7 @@ vim.cmd [[colorscheme tokyonight]]
 
 require("mason").setup()
 require("mason-lspconfig").setup {
-  ensure_installed = { "lua_ls", "rust_analyzer", "gopls", "jsonls", "yamlls" },
+  ensure_installed = { "lua_ls", "rust_analyzer", "gopls", "jsonls" },
 }
 
 --local lspconfig = require('lspconfig')
