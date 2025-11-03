9b9775ea50f3b0a7e4029b4eca530d9b02a78077
Sebastian Słaby <sebastian.slaby@hyperexponential.com>
2025-11-03 09:12:26 +0100

    gotos

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -89,9 +89,9 @@ nvim_lsp.jinja_lsp.setup {
 --local on_attach = function(client, bufnr)
 --  local opts = { noremap=true, silent=true, buffer=bufnr }
 --  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
---  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
---  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
---  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
 --end
 
 
