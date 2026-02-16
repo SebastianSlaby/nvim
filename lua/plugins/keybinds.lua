7d164d8ef5a52d7f2289dfb808bc97e4d4a984a7
Sebastian Słaby <sebastian.slaby@hyperexponential.com>
2026-02-16 10:23:26 +0100

    use tab to switch tabs

---
+++ lua/plugins/keybinds.lua
@@ -0,0 +1,19 @@
+local function is_neotree_focused() return vim.bo.filetype == "neo-tree" end
+
+vim.keymap.set("n", "<Tab>", function()
+  if is_neotree_focused() then
+    -- Switch to next source (File → Buffers → Git → File...)
+    vim.api.nvim_feedkeys(">", "n", false)
+  else
+    vim.cmd "bnext"
+  end
+end, { noremap = true, silent = true })
+
+vim.keymap.set("n", "<S-Tab>", function()
+  if is_neotree_focused() then
+    -- Switch to previous source (File → Git → Buffers → File...)
+    vim.api.nvim_feedkeys("<", "n", false)
+  else
+    vim.cmd "bprevious"
+  end
+end, { noremap = true, silent = true })
