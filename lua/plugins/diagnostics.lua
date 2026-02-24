e252b006c64f54a41d5db3d5fdbeb1890d0e8f6e
Sebastian Słaby <sebastian.slaby@hyperexponential.com>
2026-02-24 11:55:05 +0100

    diags

--- lua/plugins/diagnostics.lua
+++ lua/plugins/diagnostics.lua
@@ -5,15 +5,15 @@ return {
     features = {
       -- toggle if diagnostics are enabled on startup
       diagnostics = {
-        virtual_text = false,
-        virtual_lines = true, -- disable one option on startup
+        virtual_text = true,
+        virtual_lines = false, -- disable one option on startup
       },
     },
     -- Configuration passed to `vim.diagnostic.config()`
     -- All available options can be found with `:h vim.diagnostic.Opts`
     diagnostics = {
-      virtual_text = false,
-      virtual_lines = true, -- Neovim v0.11+ only
+      virtual_text = true,
+      virtual_lines = false, -- Neovim v0.11+ only
       update_in_insert = false,
       underline = true,
       severity_sort = true,
